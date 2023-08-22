1539dcdce1b9746d8f7cd5fbbb4c9c3ddc776ef9
Vijay <axelblaze14022000@gmail.com>
2023-08-22 12:14:56 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center">
+        Hello world!<br /><br />This is NexTrend! <br /><br /> Elevate your style, embrace the future 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
