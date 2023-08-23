ef9fd8056b7d4a6d4fe9b457703da3e44700d3dc
Nikhil <nikhilmaurya0926@gmail.com>
2023-08-23 23:29:37 +0530

    initial

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center">
-        Hello world!111<br /><br />This is nexTrend! <br /><br /> Elevate your style, embrace the future 🚀
+      <h1 className="text-3xl font-extrabold text-center text-red-500">
+        Hello world!111<br /><br />This is nexTrend! <br /><br  /> <span className="text-blue-500">Elevate your style, embrace the future 🚀</span>
       </h1>
     </div>
   )
