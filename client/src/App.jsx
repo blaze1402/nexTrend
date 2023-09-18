c6e45c35fb4153c7ec3083f81a41b9bca73d0bce
VAIBHAV RAJ <srvstvvaibhavraj@gmail.com>
2023-09-18 22:32:49 +0530

    Implemented Checkout,AddressCard,DeliveryAddressForm,OrderSummary

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import Navigation from "./components/Navigation/Navigation"
 import HomePage from "./pages/HomePage/HomePage"
-
+import Checkout from "./components/Checkout/Checkout"
+import { Route,Routes } from "react-router-dom"
+import DeliveryAddressForm from "./components/Checkout/DeliveryAddressForm"
 const App = () => {
   return (
-    <div>
+    <>
       <Navigation/>
-      <HomePage/>
-    </div>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route  path="/checkout"element={ <Checkout />}></Route>
+      </Routes>
+    </>
   )
 }
 
---
+++ client/src/components/AddressCard/AddressCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AddressCard = () => {
+  return (
+    <div>
+      <div className='space-y-2'>
+        <p className='font-semibold'>Encore ABJ</p>
+        <p>Mayur Vihar, Phase-3, 110096</p>
+        <div className='space-y-1'>
+          <p className='font-semibold'>Phone Number</p>
+          <p>9560893761</p>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddressCard
