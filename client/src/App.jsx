cf16dc88570be96979c01e4c6c829a18e3b23315
Nikhil Singh <nikhilsingh6538@gmail.com>
2023-09-26 21:20:26 +0530

    CartUpdate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import Navigation from "./components/Navigation/Navigation"
 import HomePage from "./pages/HomePage/HomePage"
+import Cart from "./components/Cart/Cart"
 
 const App = () => {
   return (
     <div>
       <Navigation/>
       <HomePage/>
+      <Cart/>
     </div>
   )
 }
 
-export default App
+export default Appclear
