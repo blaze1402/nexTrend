d6cd8ec79d2626ec1d21e920a6df3cb9f7d0bdef
Nikhil <nikhilmaurya0926@gmail.com>
2023-10-02 12:22:08 +0530

    Mini cards

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,19 @@
 // import React from 'react'
 
+import HomeSectionCarosel from "../../components/HomSectionCarosel/HomeSectionCarosel"
 import MainCarosel from "../../components/HomeCarosel/MainCarosel"
 
 function HomePage() {
   return (
     <div>
       <MainCarosel/>
-        <div>
-          other section
+        <div className="py-20 space-y-10 flex flex-col justify-center ">
+          <HomeSectionCarosel/>
+          <HomeSectionCarosel/>
+          <HomeSectionCarosel/>
+          <HomeSectionCarosel/>
+          <HomeSectionCarosel/>
+          <HomeSectionCarosel/>
         </div>
     </div> 
   )
