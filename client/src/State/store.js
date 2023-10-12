d4c4ff106dfc122f57dbd6f828e1346310d3ad1e
Vijay <axelblaze14022000@gmail.com>
2023-10-12 20:16:49 +0530

    Implemented api actions for customer product services

--- client/src/State/store.js
+++ client/src/State/store.js
@@ -2,10 +2,11 @@ import { legacy_createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import { authReducer } from './Auth/Reducer';
+import customerProductReducer from './Product/Reducer';
 
 const rootReducer = combineReducers({
-    auth: authReducer
-
+    auth: authReducer,
+    product: customerProductReducer
 });
 
 const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
--- client/src/config/apiConfig.js
+++ client/src/config/apiConfig.js
@@ -1,1 +1,15 @@
+import axios from "axios"
+
 export const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}`
+
+const jwt = localStorage.getItem("jwt")
+
+const api = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+        "Authorization": `Bearer ${jwt}`,
+        "Content-Type": "application/json"
+    }
+})
+
+export default api
